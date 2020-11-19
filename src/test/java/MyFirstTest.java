6cea6c47ccc5ac8cea612452b203a98bc64a31c2
Val <valrunco@Gmail.com>
2020-11-19 13:03:26 +0200

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, corona!");
+
+        int sum = sumTwoDigits(15, 45);
+        System.out.println(sum);
+
+        System.out.println(sumTwoDigits(9, 13));
+        
+
+
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        int c = a + b;
+        return c;
+
+    }
+}
+
+
